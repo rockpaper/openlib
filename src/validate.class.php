572ec6be29aa5d3857b84e9eb2d39d4a1f8c1ca7
JSX <saurabh@rebugged.com>
2012-09-06 07:34:42 +0530

    Added function to validate datetime format Validate::is_valid_datetime()

--- src/validate.class.php
+++ src/validate.class.php
@@ -123,4 +123,21 @@ class Validate
         }
         return $data;
     }
+    
+     /**
+     * Checks if a string is in valid date time format
+     * @param string $datetime
+     * @return boolean
+     */
+    public static function is_valid_datetime($datetime)
+    {
+        if (preg_match("/^(\d{4})-(\d{2})-(\d{2}) ([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $datetime, $matches))
+        {
+            if (checkdate($matches[2], $matches[3], $matches[1]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
